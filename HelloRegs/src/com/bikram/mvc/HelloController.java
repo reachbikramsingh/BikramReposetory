5974b81ebb4ecc11628f58e2756b86576c8f974f
reachbikramsingh <reachbikramsingh@gmail.com>
2017-12-30 16:54:44 +0530

    new code

--- HelloRegs/src/com/bikram/mvc/HelloController.java
+++ HelloRegs/src/com/bikram/mvc/HelloController.java
@@ -2,4 +2,6 @@ package com.bikram.mvc;
 
 public class HelloController {
 
+	
+	
 }
---
+++ HelloRegs/src/com/bikram/mvc/SaveController.java
@@ -0,0 +1,7 @@
+package com.bikram.mvc;
+
+public class SaveController {
+public void mapController() {
+	System.out.println("hello map controlle...");
+}
+}
