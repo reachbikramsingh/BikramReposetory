d9b86e2a75a33378d422ec3f8e317167af3a4377
reachbikramsingh <reachbikramsingh@gmail.com>
2017-12-30 16:56:12 +0530

    next

--- HelloRegs/src/com/bikram/mvc/SaveController.java
+++ HelloRegs/src/com/bikram/mvc/SaveController.java
@@ -4,4 +4,7 @@ public class SaveController {
 public void mapController() {
 	System.out.println("hello map controlle...");
 }
+public void helpController() {
+	System.out.println("hello map controlle...");
+}
 }
